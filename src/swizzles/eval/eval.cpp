569f4cbf16430ac48ae1c13ae2bb231263311caa
sgriffin53 <sgriffin53@gmail.com>
2022-04-02 10:26:35 +0100

    Added mobility for bishops, rooks, and queens

--- src/swizzles/eval/eval.cpp
+++ src/swizzles/eval/eval.cpp
@@ -1,4 +1,5 @@
 #include "eval.hpp"
+#include <chess/magic.hpp>
 #include <chess/position.hpp>
 #include "pst.hpp"
 
@@ -12,6 +13,22 @@ static constexpr std::array<Score, 6> material = {{
     {900, 900},
     {0, 0},
 }};
+
+static int bishopMgMobility[14] = {-25, -11, -6, -1, 3, 6, 9, 12, 14, 17, 19, 21, 23, 25};
+
+static int bishopEgMobility[14] = {-50, -22, -11, -2, 6, 12, 18, 24, 29, 34, 38, 42, 46, 50};
+
+static int rookMgMobility[15] = {-10, -4, -2, 0, 2, 3, 4, 5, 6, 8, 8, 9, 10, 11, 12};
+
+static int rookEgMobility[15] = {-50, -22, -11, -2, 6, 12, 18, 24, 29, 34, 38, 42, 46, 50, 54};
+
+static int queenMgMobility[28] = {-10, -6, -5, -4, -2, -2, -1, 0,  1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 6,
+
+                                  7,   7,  8,  8,  9,  9,  10, 10, 10};
+
+static int queenEgMobility[28] = {-50, -30, -22, -16, -10, -6, -2, 2,  6,  10, 13, 16, 19, 22, 24,
+
+                                  27,  30,  32,  34,  37,  39, 41, 43, 45, 47, 50, 51, 53};
 
 template <chess::Colour us>
 [[nodiscard]] auto eval_us(const chess::Position &pos) noexcept -> Score {
@@ -42,6 +59,20 @@ template <chess::Colour us>
     }
     for (const auto sq : pos.get_kings(us)) {
         score += pst_value<us>(chess::PieceType::King, sq);
+    }
+
+    // Mobility for bishops, rooks, and queens
+    for (const auto square : pos.get_bishops(us)) {
+        auto count = chess::magic::bishop_moves(square, pos.get_occupied()).count();
+        score += Score{bishopMgMobility[count], bishopEgMobility[count]};
+    }
+    for (const auto square : pos.get_rooks(us)) {
+        auto count = chess::magic::rook_moves(square, pos.get_occupied()).count();
+        score += Score{rookMgMobility[count], rookEgMobility[count]};
+    }
+    for (const auto square : pos.get_queens(us)) {
+        auto count = chess::magic::queen_moves(square, pos.get_occupied()).count();
+        score += Score{queenMgMobility[count], queenEgMobility[count]};
     }
 
     return score;
